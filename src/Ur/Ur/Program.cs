4bf1cf76741022d728a938640c6ae54cc3b35c2d
Hubert Gazda <diablo0201@wp.pl>
2024-11-08 17:01:03 +0100

    Feature/ Menu

--- src/Ur/Ur/Program.cs
+++ src/Ur/Ur/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Ur
+﻿using System.Threading.Channels;
+
+namespace Domain.Entities;
+
+class Program { 
+static void Main(string[] args)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        bool Doit = true;
+        int choice;
+        do
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+            Console.WriteLine("What do you want to do ? :");
+            if (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid choice, choose again ");
+            }
+            switch (choice) { 
+            case 1:
+
+            break;
+            };
+        } while (Doit);
+            
+            
+               
+       
+}
 }
