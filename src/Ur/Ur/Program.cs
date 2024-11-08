34f10f0ad9f5c5ac8702f6e263db68e2c9b1d313
Hubert Gazda <diablo0201@wp.pl>
2024-11-08 16:25:39 +0100

    Fixed folders

--- src/Ur/Ur/Program.cs
+++ src/Ur/Ur/Program.cs
@@ -1,12 +1,10 @@
-﻿
-namespace ManageWarehouse
+﻿namespace Ur
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            
         }
     }
 }
